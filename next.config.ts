d5de3f17aee2ced129f3a83e96ec09252001e53e
xucongyong <ironvanxv@gmail.com>
2025-03-04 13:04:34 +0800

    ad  sitemap

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,33 @@ import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
 const nextConfig: NextConfig = {
+
+    reactStrictMode: true,
+    async redirects() {
+      return [
+        {
+          source: '/en/resource',
+          destination: '/en/resources',
+          permanent: true,
+        },
+        {
+          source: '/blog/fogg_behavior_model',
+          destination: '/en/articles/fogg_behavior_model',
+          permanent: true,
+        },
+        {
+          source: '/blog/quant/vnpy_tutorial/vnpy_tutorial_0x02_futu/',
+          destination: '/en/articles/vnpy_tutorial_0x02_futu',
+          permanent: true,
+        },
+        {
+          source: '/blog/quant/vnpy_tutorial/vnpy_tutorial_0x02_futu',
+          destination: '/en/articles/vnpy_tutorial_0x02_futu',
+          permanent: true,
+        },
+      ];
+    },
+    
 };
  
 const withNextIntl = createNextIntlPlugin();
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,7 @@ export default createMiddleware(routing);
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(de|en)/:path*']
+  matcher: ['/', '/(zh|de|en)/:path*']
 };
+
+
