37d12ec68b2b536e02f9dfa06041ad52653006bb
xucongyong <ironvanxv@gmail.com>
2025-03-17 13:28:25 +0800

    backup: 2025-03-17 13-28-25

--- next.config.ts
+++ next.config.ts
@@ -12,20 +12,167 @@ const nextConfig: NextConfig = {
           permanent: true,
         },
         {
-          source: '/blog/fogg_behavior_model',
-          destination: '/en/articles/fogg_behavior_model',
+          source: '/en/',
+          destination: '/',
           permanent: true,
         },
         {
-          source: '/blog/quant/vnpy_tutorial/vnpy_tutorial_0x02_futu/',
-          destination: '/en/articles/vnpy_tutorial_0x02_futu',
+          source: '/en/',
+          destination: '/',
+          permanent: true,
+        },
+
+
+
+        {
+          source: '/top-websites/pets_and_animals',
+          destination: '/community_and_society',
           permanent: true,
         },
         {
-          source: '/blog/quant/vnpy_tutorial/vnpy_tutorial_0x02_futu',
-          destination: '/en/articles/vnpy_tutorial_0x02_futu',
+          source: '/top-websites/community_and_society',
+          destination: '/community_and_society',
           permanent: true,
         },
+        {
+          source: '/top-websites/science_and_education',
+          destination: '/science_and_education',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/lifestyle',
+          destination: '/lifestyle',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/games',
+          destination: '/games',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/vehicles',
+          destination: '/vehicles',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/computers_electronics_and_technology',
+          destination: '/computers_electronics_and_technology',
+          permanent: true,
+        },
+        
+        {
+          source: '/top-websites/heavy_industry_and_engineering',
+          destination: '/heavy_industry_and_engineering',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/adult',
+          destination: '/adult',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/sports',
+          destination: '/sports',
+          permanent: true,
+        },
+        
+        {
+          source: '/top-websites/finance',
+          destination: '/finance',
+          permanent: true,
+        },
+        
+        {
+          source: '/top-websites/travel_and_tourism',
+          destination: '/travel_and_tourism',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/health',
+          destination: '/health',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/food_and_drink',
+          destination: '/food_and_drink',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/jobs_and_career',
+          destination: '/jobs_and_career',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/reference_materials',
+          destination: '/reference_materials',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/arts_and_entertainment',
+          destination: '/arts_and_entertainment',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/business_and_consumer_services',
+          destination: '/business_and_consumer_services',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/gambling',
+          destination: '/gambling',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/ecommerce_and_shopping',
+          destination: '/ecommerce_and_shopping',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/home_and_garden',
+          destination: '/home_and_garden',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/law_and_government',
+          destination: '/law_and_government',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/medical_and_health',
+          destination: '/medical_and_health',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/real_estate',
+          destination: '/real_estate',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/technology_and_software',
+          destination: '/technology_and_software',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/travel_and_tourism',
+          destination: '/travel_and_tourism',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/weapons_and_armament',
+          destination: '/weapons_and_armament',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/other',
+          destination: '/other',
+          permanent: true,
+        },
+        {
+          source: '/top-websites/',
+          destination: '/',
+          permanent: true,
+        }
+                
       ];
     },
     
